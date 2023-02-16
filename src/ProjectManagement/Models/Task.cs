b6401044b84193ae48fa092830bc53c0a6d0b427
afroze9 <afrozeamjad@gmail.com>
2023-02-16 20:18:48 +0500

    Minor change to task-user relation

--- src/ProjectManagement/Models/Task.cs
+++ src/ProjectManagement/Models/Task.cs
@@ -5,11 +5,6 @@ namespace ProjectManagement.Models;
 [Collection("task")]
 public class Task : Entity, ICreatedOn, IModifiedOn
 {
-    public Task()
-    {
-        this.InitOneToMany(() => AssignedTo);
-    }
-
     required public string Summary { get; set; }
 
     public string? Description { get; set; }
@@ -18,7 +13,7 @@ public class Task : Entity, ICreatedOn, IModifiedOn
 
     [InverseSide] public One<Project> Project { get; set; }
 
-    [OwnerSide] public Many<Person> AssignedTo { get; set; }
+    [OwnerSide] public One<Person> AssignedTo { get; set; }
 
     public DateTime CreatedOn { get; set; }
 
